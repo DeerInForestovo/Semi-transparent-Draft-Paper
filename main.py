dbc65f6237f80eab03ba8ea968f10643cfc8a580
sustechkl <12111012@mail.sustech.edu.cn>
2024-01-05 01:55:04 +0800

    优化

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import keyboard
 
 class App:
     def __init__(self):
-        self.mode = True
+        self.mode = False
         root = Tk()
         root.attributes("-fullscreen", True)    # 全屏
-        root.attributes("-alpha", 0.15)         # 绘画模式下的遮罩颜色深度，范围0-1（可改）
         root.attributes('-topmost', 1)          # 置于顶层
         self.root = root
+        self.canvas = None
         self.build_canvas()
         keyboard.add_hotkey('f', self.root.destroy)     # F 键退出（可改）
+        keyboard.add_hotkey('alt', self.clear_canvas)   # Alt 键清空（可改）
         keyboard.add_hotkey('tab', self.switch)         # Tab 键切换显示和绘画模式（可改）
 
     def build_canvas(self):
@@ -22,22 +23,28 @@ class App:
 
         def draw_smth(event):
             global lasx, lasy
-            canvas.create_line((lasx, lasy, event.x, event.y), fill='blue', width=5)
+            self.canvas.create_line((lasx, lasy, event.x, event.y), fill='blue', width=5)
             lasx, lasy = event.x, event.y
 
-        canvas = Canvas(self.root, bg='white')
-        canvas.pack(anchor='nw', fill='both', expand=1)
-        canvas.bind("<Button-1>", get_x_and_y)      # 用左键绘画
-        canvas.bind("<B1-Motion>", draw_smth)
+        self.canvas = Canvas(self.root, bg='white')
+        self.canvas.pack(anchor='nw', fill='both', expand=1)
+        self.canvas.bind("<Button-1>", get_x_and_y)      # 用左键绘画
+        self.canvas.bind("<B1-Motion>", draw_smth)
+
+    def clear_canvas(self):
+        self.canvas.delete("all")
 
     def work(self):
+        self.switch()
         self.root.mainloop()
 
     def switch(self):
         if self.mode:
+            self.root.attributes("-alpha", 1)           # 显示模式下的画笔颜色深度，范围0-1（可改）
             self.root.wm_attributes("-transparentcolor", "white")
             self.mode = False
         else:
+            self.root.attributes("-alpha", 0.5)        # 绘画模式下的遮罩颜色深度，范围0-1（可改）
             self.root.wm_attributes("-transparentcolor", "black")
             self.mode = True
 
